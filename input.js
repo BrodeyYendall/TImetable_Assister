3b964417d4808ccb3c40056829bc41f1e7856bfd
BrodeyYendall <brodey_yendall@hotmail.com>
2018-12-27 17:09:41 +1100

    Implemented removing classes and changing colours and names of an already made class

--- input.js
+++ input.js
@@ -1,18 +1,29 @@
-var classes = []; 
+var classes = new Object(); 
 var classCount = 0; 
+var originalName = ""; 
 
-function toggleClassContents(classContainer) { 
-    let contents = classContainer.querySelector(".classHideShowContents"); 
 
-    let imageContainer = classContainer.querySelector(".classHideShowHeader").querySelector(".instructionTextWrapper"); 
-    let image = imageContainer.getElementsByTagName("img")[0]; 
+function toggleClassContents(event) { 
+    let eventTarget = event["target"];
+    if(!eventTarget.matches(".classColourChange, .classNameChange")) {
+        let classContainer = eventTarget.parentElement.parentElement;
+        if(eventTarget.className === "classHideShowHeader") {
+            classContainer = eventTarget.parentElement; 
+        } else if(eventTarget.className === "instructionText" || eventTarget.nodeName === "IMG") {
+            classContainer = classContainer.parentElement;
+        }
+        let contents = classContainer.querySelector(".classHideShowContents"); 
 
-    if(contents.style.display === "block" || contents.style.display === "") { 
-        contents.style.display = "none"
-        image.style.transform = "scaleY(1)";
-    } else { 
-        contents.style.display = "block";
-        image.style.transform = "scaleY(-1)";
+        let imageContainer = classContainer.querySelector(".classHideShowHeader").querySelector(".instructionTextWrapper"); 
+        let image = imageContainer.getElementsByTagName("img")[0]; 
+    
+        if(contents.style.display === "block" || contents.style.display === "") { 
+            contents.style.display = "none"
+            image.style.transform = "scaleY(1)";
+        } else { 
+            contents.style.display = "block";
+            image.style.transform = "scaleY(-1)";
+        }
     }
 }
 
@@ -64,17 +75,21 @@ function addClass() {
     classes[className] = classObject; 
     classCount += 1; 
 
-    document.getElementById("classList").innerHTML = visualiseClass(classObject, className) + document.getElementById("classList").innerHTML; 
+
+    let classList = document.getElementById("classList");
+    let visualisedClass = visualiseClass(classObject, className); 
+
+    classList.insertBefore(visualisedClass, classList.childNodes[0]); 
+
+    visualisedClass.querySelector(".classHideShowHeader").addEventListener("click", toggleClassContents);
 }
 
 function visualiseClass(classObject, name) { 
 
-    let colourSub = classObject["colour"].substring(1);  // Removes the # at the start 
-    let r = parseInt(colourSub.substring(0,2), 16);
-    let g = parseInt(colourSub.substring(2,4), 16);
-    let b = parseInt(colourSub.substring(4,6), 16);
+    let rgb = getRGB(classObject["colour"]); 
+    let luma = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2]; // Calculate the "brightness" (luma) of the colour with luma coefficents https://en.wikipedia.org/wiki/Rec._709#Luma_coefficients
+    let borderColour = "rgb(" + (rgb[0] - 40) + "," + (rgb[1] - 40) + "," + (rgb[2] - 40) + ")"; 
 
-    let luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // Calculate the "brightness" (luma) of the colour with luma coefficents https://en.wikipedia.org/wiki/Rec._709#Luma_coefficients
 
     let fontColour; 
     let image; 
@@ -88,21 +103,25 @@ function visualiseClass(classObject, name) {
 
     let table = classToTable(classObject["slots"]); 
 
-    let classHeaderStyle = "background-color:" + classObject["colour"] +  ";border-color: rgb(" + (r - 20) + "," + (g - 20) + "," + (b - 20) + "); display:block;"
-    let classContentStyle = "background-color: rgba(" +  r + "," + g+ "," + b + "," + (0.2) + ")" +  ";border-color: rgb(" + (r - 20) + "," + (g - 20) + "," + (b - 20) + ")";
+    let classHeaderStyle = "background-color:" + classObject["colour"] +  ";border-color: " + borderColour + ";";
+    let classNameChangeStyle = "color: " + fontColour + ";background-color: " + classObject["colour"] + ";"; 
+    let classContentStyle = "background-color: rgba(" +  rgb[0] + "," + rgb[1] + "," + rgb[2] + "," + (0.2) + ")" +  ";border-color: " + borderColour + ";";
 
-    return "\
-    <div class='classContainer'>\
-        <div class='classHideShowHeader' onclick='toggleClassContents(this.parentElement)' style='" + classHeaderStyle + "'>\
-            <div class='className' style='color: " + fontColour + "'>" + name +"</div>\
-            <div class='instructionTextWrapper'>\
-                <span class='instructionText' style='color: " + fontColour + "'>Click to show more </span><img src='media/" + image + "'/>\
-            </div>\
+    let visualisedOjbect = document.createElement("div");
+    visualisedOjbect.className = "classContainer";   
+    visualisedOjbect.innerHTML = "\
+    <div class='classHideShowHeader' style='" + classHeaderStyle + "'>\
+        <input class='classNameChange' style='" + classNameChangeStyle + "' type='text' maxlength='30' value='" + name + "' onfocus='getOriginalName(this);' onchange='changeName(this);'>\
+        <input type='color' class='classColourChange' onchange='changeClassColour(this.parentElement, this);' value='" + classObject["colour"] + "' style='border-color:"+fontColour +";'>\
+        <div class='instructionTextWrapper'>\
+            <span class='instructionText' style='color: " + fontColour + "'>Click to show more </span><img src='media/" + image + "'/>\
         </div>\
-        <div class='classHideShowContents' style='" + classContentStyle + "'>\
-            " + table + "\
-        </div>\
-    </div>";
+    </div>\
+    <img class='removeButton' src='media/remove.png' onclick='removeObject(this.parentElement);'/>\
+    <div class='classHideShowContents' style='" + classContentStyle + "'>\
+        " + table + "\
+    </div>"; 
+    return visualisedOjbect; 
 }
 
 
@@ -121,11 +140,77 @@ function listTest() {
     }
 }
 
+function removeObject(classContainer) {
+    let className = classContainer.querySelector(".classHideShowHeader").querySelector(".classNameChange").value;
+    delete classes[className]; 
+    document.getElementById("classList").removeChild(classContainer);
+}
+
+function changeClassColour(classHeader, newColour) { 
+    let className = classHeader.querySelector(".classNameChange"); 
+    classes[className.value].colour = newColour.value; 
+
+    let rgb = getRGB(newColour.value); 
+    let luma = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2]; // Calculate the "brightness" (luma) of the colour with luma coefficents https://en.wikipedia.org/wiki/Rec._709#Luma_coefficients
+    let borderColour = "rgb(" + (rgb[0] - 40) + "," + (rgb[1] - 40) + "," + (rgb[2] - 40) + ")"; 
+
+    let fontColour; 
+    let image; 
+    if(luma < 180) {    
+        fontColour = "white";
+        image = "arrowLight.png"
+    } else {
+        fontColour = "#636363"; // Colour used for black text, causes the text to appear more warm  
+        image = "arrowDark.png"
+    }
+
+    newColour.style.borderColor = fontColour;
+
+    classHeader.style.borderColor = borderColour;
+    classHeader.style.backgroundColor = newColour.value;
+
+    let classNameChange = classHeader.querySelector(".classNameChange"); 
+    classNameChange.style.backgroundColor = newColour.value;
+    classNameChange.style.color = fontColour; 
+
+    let instructionTextWrapper = classHeader.querySelector(".instructionTextWrapper"); 
+    instructionTextWrapper.querySelector(".instructionText").style.color = fontColour; 
+    instructionTextWrapper.getElementsByTagName("img")[0].src = "media/" + image; 
+    
+    classHeader.parentElement.querySelector(".classHideShowContents").style = "background-color: rgba(" +  rgb[0] + "," + rgb[1] + "," + rgb[2] + "," + (0.2) + ")" +  ";border-color: " + borderColour + ";";
+
+    console.log(classes); 
+}
+
+function getRGB(colour) {
+    rgb = []
+    if(colour.charAt(0) === "#") { 
+        colour = colour.substring(1);  // Removes the # at the start
+    }
+     
+    rgb.push(parseInt(colour.substring(0,2), 16));
+    rgb.push(parseInt(colour.substring(2,4), 16));
+    rgb.push(parseInt(colour.substring(4,6), 16));
+    return rgb; 
+}
+
 function compareObjects(object1, object2) {; 
     if(JSON.stringify(object1) === JSON.stringify(object2) ) {
         return true;
     } else {
         return false; 
     }
+}
+
+function getOriginalName(nameContainer) {
+    console.log("Preparing name");
+    originalName = nameContainer.value; 
+}
+
+function changeName(nameContainer) {
+    let newName = nameContainer.value; 
+    let classbject = classes[originalName];
+    delete classes[originalName]; 
+    classes[nameContainer.value] = classbject;
 }
 
